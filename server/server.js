8c286a819c132966dc4b83116a586c95db3bc4d6
khalid hagane <khalidhagane62@gmail.com>
2023-01-24 20:25:51 +0100

    ajoute foction errorhandler pour handler les error

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const dbConnect = require('./config/config')
 const express = require('express')
+const errorHandler = require('./middlwares/errorHandler')
 
 dbConnect()
 
@@ -10,6 +11,8 @@ const app = express()
 const pharmacyRouter = require('./routes/pharmacy')
 
 app.use('/api/pharmacy', pharmacyRouter)
+
+app.use(errorHandler)
 
 app.listen(process.env.PORT, () => console.log('Server running on port 5000') )
 
