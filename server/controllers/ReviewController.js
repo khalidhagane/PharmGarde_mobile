1e56c339bf6a5e03e94ab312718efd4578d46edb
NoureddineMa <noureddinemaher13@gmail.com>
2023-01-24 20:41:43 +0100

    getall reviews for a pharmacy

--- server/controllers/ReviewController.js
+++ server/controllers/ReviewController.js
@@ -1,13 +1,10 @@
-const { findOne } = require('../models/Review');
-const { Pharmacy } = require('../controllers/pharmacyController')
 const Review = require('../models/Review');
 
-// @desc POST Single Client:
+// @desc POST Single Review for a Pharmacy :
 // @route POST http://localhost:5000/api/review/:id
 // @access Public
 
 const createReview = async (req, res) => {
-    
     const ReviewData = await Review.create({
         review: req.body.review,
         id_Pharmacy: req.params.id
@@ -21,8 +18,20 @@ const createReview = async (req, res) => {
     }
 }
 
-const getAllReviewsByPharmacyID = (req, res) => {
-    res.send('getAll Reviews Work!')
+// @desc GET ALL Reviews by id of the Pharmacy:
+// @route GET http://localhost:5000/api/reviews/:id
+// @access Private
+
+const getAllReviewsByPharmacyID = async (req, res) => {
+    const id = req.params.id;
+    const AllReviewsPharmacy = await Review.find({id_Pharmacy: id})
+    if(AllReviewsPharmacy){
+        res.status(200)
+        .json(AllReviewsPharmacy)
+    } else {
+        res.status(400)
+        .json({message: "No Reviews :( "})
+    }
 }
 
-module.exports = { createReview, getAllReviewsByPharmacyID }
+module.exports = { createReview, getAllReviewsByPharmacyID }    
