0d8b946e44030840891939ad7450cd3ce029a31b
NoureddineMa <noureddinemaher13@gmail.com>
2023-01-27 09:16:07 +0100

    styling layout

--- frontend/web/src/pages/Layout.jsx
+++ frontend/web/src/pages/Layout.jsx
@@ -6,7 +6,7 @@ import Logout from '../components/Logout'
 function Layout() {
     return (
         <div className="antialiased  w-full min-h-screen text-slate-300 relative py-4 bg-[#AEE2FF]">
-            <div className="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
+            <div className="grid grid-cols-12  gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14  my-10 px-2">
                 <div id="menu" className="bg-[#93C6E7] col-span-3 rounded-lg p-4 ">
                     <Link to='/Dashboard'>
                         <h1 className="font-bold text-lg ml-7 my-5 lg:text-3xl bg-gradient-to-br  via-white/50 to-transparent bg-clip-text text-[#00337C]">Dashboard<span className="text-[#00337C]">.</span></h1>
@@ -85,7 +85,7 @@ function Layout() {
 
                     </div>
                 </div>
-                <div id="content" className="bg-[#93C6E7] col-span-9 rounded-lg p-6">
+                <div id="content" className=" col-span-9 rounded-lg p-6">
                     <div className='bg-[#93C6E7] py-4 px-4 rounded-lg'>
                         <Outlet />
                     </div>
