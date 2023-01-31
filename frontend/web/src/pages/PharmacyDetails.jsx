7c3197b8307c842c00a4ada946aa3348c20c24ef
NoureddineMa <noureddinemaher13@gmail.com>
2023-01-31 09:14:17 +0100

    consommation api get comments pharmcies

--- frontend/web/src/pages/PharmacyDetails.jsx
+++ frontend/web/src/pages/PharmacyDetails.jsx
@@ -140,19 +140,23 @@ function PharmacyDetails() {
                     </div>
                 </div>
                 <div className="flex justify-center">
-                
+                {comments.map((comment,i) =>
 <article className="flex content my-5 rounded-lg bg-white transition hover:shadow-xl">
-  <div className="flex flex-1 flex-col justify-between">
-    <img src="https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg" alt="" />
-    {comments.map((comment,i) => 
-    <div className="border-l border-gray-900/10 p-4 sm:border-l-transparent sm:p-6">  
+  <div className="flex flex-1 flex-col lg:flex-row center">
+    <div className="border-l border-gray-900/10 p-4 sm:border-l-transparent sm:p-6 flex items-center">  
+    <img className="image" src="https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg" alt="" />
       <p className="mt-2 text-sm leading-relaxed text-gray-700 line-clamp-3" key={i}>
         {comment.comment}
       </p>
     </div>
-    )}
+    <Link to="/test" className="mr-4">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.5 15.5L15.5 8.5M15.5 15.5L8.5 8.5M14.9 2H9.1C8.42 2 7.46 2.4 6.98 2.88L2.88 6.98C2.4 7.46 2 8.42 2 9.1V14.9C2 15.58 2.4 16.54 2.88 17.02L6.98 21.12C7.46 21.6 8.42 22 9.1 22H14.9C15.58 22 16.54 21.6 17.02 21.12L21.12 17.02C21.6 16.54 22 15.58 22 14.9V9.1C22 8.42 21.6 7.46 21.12 6.98L17.02 2.88C16.54 2.4 15.58 2 14.9 2Z" stroke="#FF0000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+</Link>
   </div>
 </article>
+    )}
 </div>
             </div>
         </>
