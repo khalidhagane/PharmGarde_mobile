4b8a727e86b5cfd5c837daadde55694552ceab2d
abdessamadbettal <abdessamadbettal@gmail.com>
2023-02-09 10:29:51 +0100

    add pharmacie details

--- frontend/mobile/screens/PharmacieDetails.js
+++ frontend/mobile/screens/PharmacieDetails.js
@@ -12,44 +12,12 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 const data = [
   {
     id: 1,
-    name: 'Pharmacie 11',
-    address: 'Rue 1',
-    phone: '12345678',
+    name: 'Pharmacie E-Zarwali',
+    address: 'Rue 1 quartier elbahraine n33',
+    phone: '+2126-34-56-78-90',
     email: 'pharmacia@gmail.com',
     img: require('../assets/images/pharmacie1.jpg'),
-    reviews: [
-      {
-        id: 1,
-        name: 'User 1',
-        rating: 5,
-        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget ultricies nunc nunc vel nunc. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget ultricies nunc nunc vel nunc.'
-      },
-      {
-        id: 2,
-        name: 'User 2',
-        rating: 4,
-        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget ultricies nunc nunc vel nunc. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget ultricies nunc nunc vel nunc.'
-      },
-      {
-        id: 3,
-        name: 'User 3',
-        rating: 3,
-        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget ultricies nunc nunc vel nunc. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget ultricies nunc nunc vel nunc.'
-      },
-      {
-        id: 4,
-        name: 'User 4',
-        rating: 2,
-        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget ultricies nunc nunc vel nunc. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget ultricies nunc nunc vel nunc.'
-      },
-      {
 
-        id: 5,
-        name: 'User 5',
-        rating: 1,
-        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget ultricies nunc nunc vel nunc. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nisl, eget ultricies nunc nunc vel nunc.'
-      },
-    ]
   }
 ]
 
@@ -57,237 +25,49 @@ const data = [
 
 PharmacieDetails = () => {
   return (
-    <View style={
-      {
-        flex: 1,
-        // backgroundColor: '#aaa', 
-        alignItems: 'center',
-        // paddingBottom: 60,
-
-        // justifyContent: 'center',
-      }
-    }>
-      <Text
-        style={
-          {
-            fontSize: 30,
-            color: 'green',
-            fontWeight: 'bold',
-            paddingTop: 40,
-            paddingBottom: 20,
-          }
-        }
-      >Pharmacie Details</Text>
-      <View style={
-        {
-          flex: 1,
-          // backgroundColor: '#fff', 
-          alignItems: 'center',
-          // justifyContent: 'center',
-          width: '100%',
-          height: '100%',
-          // paddingTop: 60,
-        }
-      }>
+    <View style={{  flex: 1,  alignItems: 'center',}}>
+      <Text style={{  fontSize: 30, color: 'green', fontWeight: 'bold', paddingTop: 20, paddingBottom: 20,} }>Pharmacie Details</Text>
+      <View style={{  flex: 1, alignItems: 'center', width: '100%', height: '100%',}}>
         {/* image */}
         <Image
-          style={
-            {
-              width: 400,
-              height: 200,
-              borderRadius: 20,
-            }
-          }
+          style={ { width: 400, height: 200, borderRadius: 20,}  }
           source={data[0].img}
         />
-        <View style={
-          {
-            flex: 1,
-
-            backgroundColor: 'aliceblue',
-            // backgroundColor: 'powderblue',
-            alignItems: 'center',
-            // justifyContent: 'center',
-            width: '100%',
-            height: '100%',
-            // paddingTop: 60,
-          }
-        }>
-          <Text
-            style={
-              {
-                fontSize: 20,
-                color: 'green',
-                fontWeight: 'bold',
-                paddingTop: 40,
-                paddingBottom: 20,
-              }
-            }
-          >{data[0].name}</Text>
-          <Text
-            style={
-              {
-                paddingBottom: 40,
-              }
-            }
-
-          >
-            <Icon
-              name='star'
-              type='font-awesome'
-              color='gold'
-            />
-            <Icon
-              name='star'
-              type='font-awesome'
-              color='gold'
-            />
-            <Icon
-              name='star'
-              type='font-awesome'
-              color='gold'
-            />
-            <Icon
-              name='star'
-              type='font-awesome'
-              color='gold'
-            />
-            <Icon
-              name='star'
-              type='font-awesome'
-              color='gold'
-            />
+        <View style={{flex: 1,backgroundColor: 'aliceblue',alignItems: 'center',width: '100%',height: '100%'}}>
+          <Text style={{fontSize: 20,color: 'green',fontWeight: 'bold', paddingTop: 40,paddingBottom: 20,}}>{data[0].name}</Text>
+          <Text style={{  paddingBottom: 10,}}>
+            <Icon name='star' type='font-awesome' color='gold'/>
+            <Icon  name='star'  type='font-awesome'  color='gold'/>
+            <Icon  name='star'  type='font-awesome'  color='gold'/>
+            <Icon  name='star'  type='font-awesome'  color='gold'/>
+            <Icon  name='star'  type='font-awesome'  color='gold'/>
           </Text>
-          <Text
-            style={
-              {
-
-                fontSize: 15,
-                color: 'green',
-                fontWeight: 'bold',
-                paddingBottom: 20,
-              }
-            }
-          >{data[0].address}</Text>
-          <Text
-            style={
-              {
-
-                fontSize: 15,
-                color: 'green',
-                fontWeight: 'bold',
-                paddingBottom: 20,
-              }
-            }
-          >{data[0].phone}</Text>
-          <Text
-            style={
-              {
-                fontSize: 15,
-                color: 'green',
-                fontWeight: 'bold',
-                paddingBottom: 20,
-              }
-            }
-          >{data[0].email}</Text>
-
-        </View>
-        
-        {/* add comments */}
-        <View style={
-          {
-            flex: 1,
-            backgroundColor: 'aliceblue',
-            // backgroundColor: 'powderblue',
-            alignItems: 'center',
-            // justifyContent: 'center',
-            width: '100%',
-            height: '100%',
-            // paddingTop: 60,
-          }
-        }>
-          <Text
-            style={
-              {
-
-                fontSize: 15,
-                color: 'green',
-                fontWeight: 'bold',
-                paddingBottom: 20,
-              }
-            }
-          >Comments</Text>
-          <View style={
-            {
-              flex: 1,
-              backgroundColor: 'aliceblue',
-              // backgroundColor: 'powderblue',
-              alignItems: 'center',
-              // justifyContent: 'center',
-              width: '100%',
-              height: '100%',
-              // paddingTop: 60,
-            }
-          }>
-            <FlatList
-              data={data[0].reviews}
-              renderItem={({ item }) => (
-                <View style={
-                  {
-                    flex: 1,
-                    backgroundColor: 'aliceblue',
-                    // backgroundColor: 'powderblue',
-                    alignItems: 'center',
-                    // justifyContent: 'center',
-                    width: '100%',
-                    height: '100%',
-                    // paddingTop: 60,
-                  }
-                }>
-                  <Text
-                    style={
-                      {
-                        fontSize: 15,
-                        color: 'green',
-                        fontWeight: 'bold',
-                        paddingBottom: 20,
-                      }
-                    }
-                  >{item.name}</Text>
-                  <Text
-                    style={
-                      {
-                        fontSize: 15,
-                        color: 'green',
-                        fontWeight: 'bold',
-                        paddingBottom: 20,
-                      }
-                    }
-                  >{item.review}</Text>
-
-                </View>
-              )}
-            />
-
-            </View>
-
-
-          </View>
-
-
+          <Text style={{ fontSize: 15,  color: 'green',  fontWeight: 'bold',  paddingBottom: 20,}} >{data[0].address}</Text>
+          <Text style={{ fontSize: 15,  color: 'green',  fontWeight: 'bold',  paddingBottom: 20,}} >{data[0].phone}</Text>
+          <Text style={{  fontSize: 15,  color: 'green',  fontWeight: 'bold',  paddingBottom: 20,}} >{data[0].email}</Text>
         </View>
       </View>
+    </View>
 
 
 
 
 
 
-      )
+  )
 
 
 
 }
 
 
-      export default PharmacieDetails
+export default PharmacieDetails
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
